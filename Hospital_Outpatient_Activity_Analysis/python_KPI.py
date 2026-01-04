b41b824bece2686ec32d7304c7b9d1e998577829
mehry-lab <53804261+mehry-lab@users.noreply.github.com>
2026-01-05 09:38:22 +1100

    Update python_KPI.py

--- Hospital_Outpatient_Activity_Analysis/python_KPI.py
+++ Hospital_Outpatient_Activity_Analysis/python_KPI.py
@@ -4,7 +4,12 @@ import numpy as np
 db=pd.read_csv ("../outpatient_activity_cleaned.csv")
 total=len(db)
 
-completed=db["is_completed"].sum()
-no_show = df["is_no_show"].sum()
-cancelled = df["is_cancelled"].sum()
+Completed=db["is_completed"].sum(),
+no_show=db["is_no_show"].sum(),
+Cancelled=db["is_cancelled"].sum(),
+
+no_show_rate = no_show / total
+cancel_rate = cancelled / total
+avg_wait=db.loc[db["is_completed"]==True, "wait
+
 
